f3265221b73421cfd3fdd2c79e9c26c3182bfbe6
Benjamin <3benknight@gmail.com>
2024-08-05 15:00:07 -0400

    Fixed error with dropdown search bars not working on datasets with global regions or aggregated subsectors.

--- src/components/dropdowns/DropdownSearchBar.jsx
+++ src/components/dropdowns/DropdownSearchBar.jsx
@@ -4,13 +4,23 @@ import { Form } from 'react-bootstrap';
 export const DropdownSearchBar = React.forwardRef(
   ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
     const [value, setValue] = useState('');
-    const dropdown = (children[0].key) ? (children.filter(
-      (child) =>
-        !value || !child.key || child.key.toLowerCase().startsWith(value.toLowerCase())
-    )) : (children[1].filter(
-      (child) =>
-        !value || !child.key || child.key.toLowerCase().startsWith(value.toLowerCase())
-    ))
+    const getItems = () => {
+      if ((children && children.length === 1)|| (children && children.length === 2 && children[1].key))
+        return children;
+      if (!children || !children[0] || !children[1])
+        return "";
+      if (children[0].key)
+        return children.filter(
+          (child) =>
+            !value || !child.key || child.key.toLowerCase().startsWith(value.toLowerCase())
+        );
+      if (children[1].key)
+        return children[1].filter(
+          (child) =>
+            !value || !child.key || child.key.toLowerCase().startsWith(value.toLowerCase())
+        )
+      return "";
+    };
     return (
       <div
         ref={ref}
@@ -18,15 +28,15 @@ export const DropdownSearchBar = React.forwardRef(
         className={className}
         aria-labelledby={labeledBy}
       >
-        <Form.Control
+        {((children && children.length === 1)|| (children && children.length === 2 && children[1].key)) ? <div></div> : <Form.Control
           autoFocus
           className="mx-3 my-2 w-auto"
           placeholder="Type to filter."
           onChange={(e) => setValue(e.target.value)}
           value={value}
-        />
+        />}
         <ul className="list-unstyled">
-          {(children && children[0]) ? dropdown : ""}
+          {getItems()}
         </ul>
       </div>
     );
